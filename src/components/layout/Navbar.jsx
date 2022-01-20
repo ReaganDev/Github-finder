cd393a9c6557d9e88d8f1931810a0cfab94025c3
Reagan Reuben <reagan.reuben@decagon.dev>
2022-01-20 15:53:41 +0100

    Navbar created

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,10 +1,28 @@
 import { Link } from 'react-router-dom'
 import { FaGithub } from 'react-icons/fa'
 
-const Navbar = ({ title }) => {
+const Navbar = () => {
 	return (
 		<nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>
-			<div className='container mx-auto'>Navbar</div>
+			<div className='container mx-auto'>
+				<div className='flex-none px-2 mx-2'>
+					<FaGithub className='pr-2 inline text-3xl' />
+					<Link to='/' className='text-lg align-middle font-bold'>
+						GithubFinder
+					</Link>
+				</div>
+
+				<div className='flex-1 px-2 mx-2'>
+					<div className='flex justify-end'>
+						<Link to='/' className='btn btn-ghost btn-sm rounded-btn'>
+							Home
+						</Link>
+						<Link to='/about' className='btn btn-ghost btn-sm rounded-btn'>
+							About
+						</Link>
+					</div>
+				</div>
+			</div>
 		</nav>
 	)
 }

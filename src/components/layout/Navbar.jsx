96c03acfcdd01c7d0b568eeda9b95e4624a531af
Reagan Reuben <reagan.reuben@decagon.dev>
2022-01-20 15:40:25 +0100

    Tailwind and daisy installed

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+import { FaGithub } from 'react-icons/fa'
+
+const Navbar = ({ title }) => {
+	return (
+		<nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>
+			<div className='container mx-auto'>Navbar</div>
+		</nav>
+	)
+}
+
+export default Navbar

f1d5b82e0cf7017d423b2a10df209700abd86675
Reagan Reuben <reagan.reuben@decagon.dev>
2022-01-20 17:27:34 +0100

    Home,error,about pages created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Error from './pages/Error'
 import Footer from './components/layout/Footer'
 import Navbar from './components/layout/Navbar'
 
 function App() {
 	return (
 		<Router>
-        <div className="flex flex-col justify-between h-screen">
-          <Navbar />
-        <main className='container mx-auto px-3 pb-12'>Content</main>
-        <Footer />
-        </div>
+			<div className='flex flex-col justify-between h-screen'>
+				<Navbar />
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path='/' element={ <Home />}/>
+            <Route path='/about' element={ <About />}/>
+            <Route path='/error' element={ <Error />}/>
+            <Route path='/*' element={ <Error />}/>
+          </Routes>
+        </main>
+				<Footer />
+			</div>
 		</Router>
 	)
 }
---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+
+
+const About = () => {
+    return (
+			<div>
+				<h1 className='mb-4 text-6xl'>Github finder</h1>
+				<p className='text-2xl font-light text-gray-400'>
+					I created this application with the help of Brad Traversy's <span className="text-4xl font-dark italic">React Front to back Course.</span> Thanks Brad
+				</p>
+			</div>
+		)
+}
+
+
+
+export default About

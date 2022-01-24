b0324082079d3911a1a270b1cc289caba289cb2c
Reagan Reuben <reagan.reuben@decagon.dev>
2022-01-24 20:04:39 +0100

    alert reducer

--- src/App.js
+++ src/App.js
@@ -5,24 +5,27 @@ import Error from './pages/Error'
 import Footer from './components/layout/Footer'
 import Navbar from './components/layout/Navbar'
 import { GithubProvider } from './context/github/GithubContext'
+import { AlertProvider } from './context/alert/AlertContext'
 
 function App() {
 	return (
 		<GithubProvider>
-			<Router>
-				<div className='flex flex-col justify-between h-screen'>
-					<Navbar />
-					<main className='container mx-auto px-3 pb-12'>
-						<Routes>
-							<Route path='/' element={<Home />} />
-							<Route path='/about' element={<About />} />
-							<Route path='/error' element={<Error />} />
-							<Route path='/*' element={<Error />} />
-						</Routes>
-					</main>
-					<Footer />
-				</div>
-			</Router>
+			<AlertProvider>
+				<Router>
+					<div className='flex flex-col justify-between h-screen'>
+						<Navbar />
+						<main className='container mx-auto px-3 pb-12'>
+							<Routes>
+								<Route path='/' element={<Home />} />
+								<Route path='/about' element={<About />} />
+								<Route path='/error' element={<Error />} />
+								<Route path='/*' element={<Error />} />
+							</Routes>
+						</main>
+						<Footer />
+					</div>
+				</Router>
+			</AlertProvider>
 		</GithubProvider>
 	)
 }

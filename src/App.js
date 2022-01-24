4494a349b2381d141df2965f1ef5cfa29c4dcf62
Reagan Reuben <reagan.reuben@decagon.dev>
2022-01-24 14:15:55 +0100

    setup github context

--- src/App.js
+++ src/App.js
@@ -4,23 +4,26 @@ import About from './pages/About'
 import Error from './pages/Error'
 import Footer from './components/layout/Footer'
 import Navbar from './components/layout/Navbar'
+import { GithubProvider } from './context/github/GithubContext'
 
 function App() {
 	return (
-		<Router>
-			<div className='flex flex-col justify-between h-screen'>
-				<Navbar />
-        <main className='container mx-auto px-3 pb-12'>
-          <Routes>
-            <Route path='/' element={ <Home />}/>
-            <Route path='/about' element={ <About />}/>
-            <Route path='/error' element={ <Error />}/>
-            <Route path='/*' element={ <Error />}/>
-          </Routes>
-        </main>
-				<Footer />
-			</div>
-		</Router>
+		<GithubProvider>
+			<Router>
+				<div className='flex flex-col justify-between h-screen'>
+					<Navbar />
+					<main className='container mx-auto px-3 pb-12'>
+						<Routes>
+							<Route path='/' element={<Home />} />
+							<Route path='/about' element={<About />} />
+							<Route path='/error' element={<Error />} />
+							<Route path='/*' element={<Error />} />
+						</Routes>
+					</main>
+					<Footer />
+				</div>
+			</Router>
+		</GithubProvider>
 	)
 }
 

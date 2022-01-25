7debeb66ca7cdc3b47477339f6f1f3d644110f33
Reagan Reuben <reagan.reuben@decagon.dev>
2022-01-25 17:18:13 +0100

    Edit about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,13 @@
-
-
 const About = () => {
-    return (
-			<div>
-				<h1 className='mb-4 text-6xl'>Github finder</h1>
-				<p className='text-2xl font-light text-gray-400'>
-					I created this application with the help of Brad Traversy's <span className="text-4xl font-dark italic">React Front to back Course.</span> Thanks Brad
-				</p>
-			</div>
-		)
+	return (
+		<div>
+			<h1 className='mb-4 text-6xl'>Github finder</h1>
+			<p className='text-2xl font-light text-gray-400'>
+				Find Github users and check out their profiles,latest
+				repositories,forks,gists e.t.c
+			</p>
+		</div>
+	)
 }
-
-
 
 export default About
